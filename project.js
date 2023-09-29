816b668f827482c339fccd0da6ba5dc449f68445
Dynielle Ryan Q.Goc-ong <gocong.dynielleryan@gmail.com>
2023-09-29 18:24:02 +0800

    Latest version

--- project.js
+++ project.js
@@ -8,6 +8,22 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;    //common practice for a global variable that is a constant to be all caps.
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+     A: 2,
+     B: 4,
+     C: 6,
+     D: 8
+}
+
+const SYMBOL_VALUES = {
+     A: 5,
+     B: 4,
+     C: 3,
+     D: 2
+}
 
 
 const deposit = () => {
@@ -48,6 +64,11 @@ const getBet = (balance) => {
         }
     }
 };
+
+const spin = () => {
+    const symbols = [];
+    for   
+}
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();

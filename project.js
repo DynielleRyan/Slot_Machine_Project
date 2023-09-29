efcbaaa8de0e709c684d64066331598dd2b0eeb9
Dynielle Ryan Q.Goc-ong <gocong.dynielleryan@gmail.com>
2023-09-29 17:49:27 +0800

    Latest version

--- project.js
+++ project.js
@@ -8,30 +8,13 @@
 
 const prompt = require("prompt-sync")();
 
-const ROWS = 3;
-const COLS = 3;
-
-const SYMBOLS_COUNT = {
-    A : 2,
-    B : 4,
-    C : 6,
-    D : 8
-}
-
-const SYMBOL_VALUES = {
-    A : 5,
-    B : 4,
-    C : 3,
-    D : 2
-}
-
 const deposit = () => {
-     while (true){
+    while (true) {
         const depositAmount = prompt("Enter a deposit amount: ");
-        const numberDepositAmount = parseFloat(depositAmount);
+        const numberDepositAmount = parseFloat(depositAmount); //Parse is to convert a string to some value
 
-        if (isNaN(numberDepositAmount)|| numberDepositAmount <= 0){
-            console.log("Invalid deposit amount, try again.")
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount, try again.");
         }else{
             return numberDepositAmount;
         }
@@ -39,47 +22,33 @@ const deposit = () => {
 };
 
 const getNumberOfLines = () => {
-    while (true){
-        const lines = prompt("Enter the amount of lines to bet on (1-3): ");
-        const numberOfLines = parseFloat(lines);
-    
-        if (isNaN(numberOfLines)|| numberOfLines <= 0 || numberOfLines > 3){
-            console.log("Invalid number of lines, try again.")
+    while (true) {
+        const lines = prompt("enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines); //parse is to convert a string to an actual number
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid number of lines, try again.");
         }else{
             return numberOfLines;
         }
-      }
-};
+    }
+}
 
-const getBet = (balance, lines) => {
-    while (true){
-        const bet = prompt("Enter the bet per line: ");
-        const numberBet = parseFloat(bet);
-    
-        if (isNaN(numberBet)|| numberBet <= 0 || numberBet > balance / lines){
-            console.log("Invalid bet amount, try again")
+const getBet = (balance) => {
+    while (true) {
+        const bet = prompt ("enter the total  bet: ");
+        const numberBet = parseFloat(bet); //parse is to convert a string to an actual number
+
+        if(isNaN(numberBet) || numberBet > balance || numberBet <= 0){
+            console.log("Please try again");
         }else{
             return numberBet;
         }
-      }
-};
-
-const spin = () => {
-    const symbols = [];
-    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
-        console.log(symbol,count);
-        for(let i = 0; i < count; i++){
-            symbols.push(symbol);
-
-        }
     }
-    console.log(symbols);
-}
-spin();
-
+};
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
-const bet = getBet(balance,numberOfLines);
+const bet = getBet(balance);
 
 
